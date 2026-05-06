52695cbd95701a09534e3928a46c45b0a8394112
IfaGirmae <girmaiefa@icloud.com>
2026-05-06 16:37:59 -0600

    fixed sonarcloud issue

--- src/renderer.js
+++ src/renderer.js
@@ -531,7 +531,9 @@ function _animateParticles() {
 
     if (p.x <= 0 || p.x >= window.innerWidth) p.vx *= -1;
     if (p.y <= 0 || p.y >= window.innerHeight) p.vy *= -1;  
-        if (!p.recycle) p.opacity -= 0.012; 
+    if (!p.recycle) {
+        p.opacity -= 0.012;
+    }
         _particleCtx.beginPath();
         _particleCtx.arc(p.x, p.y, p.size, 0, Math.PI * 2);
         _particleCtx.fillStyle = `rgba(255, 255, 255, ${Math.max(0, p.opacity)})`;
